1849125c1afbf596b659f023fe77561da3c2bab0
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2023-02-08 21:25:55 +0600

    expand crud with findall

--- src/main/java/kz/yertayev/redbootcamp/services/IAnnouncementService.java
+++ src/main/java/kz/yertayev/redbootcamp/services/IAnnouncementService.java
@@ -6,5 +6,6 @@ import kz.yertayev.redbootcamp.model.announcement.AnnouncementDto;
 public interface IAnnouncementService {
   void createAnnouncement(AnnouncementDto dto);
   Set<AnnouncementDto> getAnnouncements();
+  void takeBid(Long id, double price);
   void takeOffAnnouncement(Long id);
 }
--- src/main/java/kz/yertayev/redbootcamp/services/impl/AnnouncementServiceImpl.java
+++ src/main/java/kz/yertayev/redbootcamp/services/impl/AnnouncementServiceImpl.java
@@ -1,10 +1,10 @@
 package kz.yertayev.redbootcamp.services.impl;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 import kz.yertayev.redbootcamp.domain.entities.AnnouncementEntity;
 import kz.yertayev.redbootcamp.domain.entities.UserEntity;
 import kz.yertayev.redbootcamp.domain.repositories.AnnouncementRepository;
-import kz.yertayev.redbootcamp.model.UserDto;
 import kz.yertayev.redbootcamp.model.announcement.AnnouncementDto;
 import kz.yertayev.redbootcamp.model.announcement.AnnouncementState;
 import kz.yertayev.redbootcamp.services.IAnnouncementService;
@@ -12,8 +12,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +23,7 @@ public class AnnouncementServiceImpl implements IAnnouncementService {
   private final ModelMapper modelMapper;
   
   @Override
+  @Transactional
   public void createAnnouncement(AnnouncementDto dto) {
     dto.setState(AnnouncementState.ACTIVE);
     announcementRepository.save(convertToEntity(dto));
@@ -30,7 +31,14 @@ public class AnnouncementServiceImpl implements IAnnouncementService {
 
   @Override
   public Set<AnnouncementDto> getAnnouncements() {
-    return null;
+    return announcementRepository.findAll().stream()
+        .map(this::convertToDto)
+        .collect(Collectors.toSet());
+  }
+
+  @Override
+  public void takeBid(Long id, double price) {
+
   }
 
   @Override
