e185cd1b1d70a7bfaa3dd288c7967b65cfc897d8
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2023-02-08 14:32:55 +0600

    Initial commit

---
+++ src/test/java/kz/yertayev/redbootcamp/RedbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yertayev.redbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbootcampApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
